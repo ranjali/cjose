1993beea56be03de0f0107eb91bff08f84e102b8
dipta <hazratpradipta.ranjali@thalesgroup.com>
2024-11-11 18:18:04 +0800

    add vscode for debugging

--- configureMac.sh
+++ configureMac.sh
@@ -1,3 +1,4 @@
-./configure CFLAGS="-g -O0 -DDEBUG -I/usr/local/include" --with-openssl=/opt/homebrew/opt/openssl --with-jansson=/opt/homebrew/opt/jansson
+./configure CFLAGS="-g3 -DDEBUG -I/usr/local/include" --host=arm64-apple-darwin --with-openssl=/opt/homebrew/opt/openssl --with-jansson=/opt/homebrew/opt/jansson
 
-echo "if it fails, run: autoreconf -fi"
+echo "if it fails, run: 'autoreconf -fi' before ./configure"
+echo "take note to install with 'sudo make install-exec' before running the test" 
